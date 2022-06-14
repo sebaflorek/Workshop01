9af94401b8ad6d60acd970bc162a89e084e0b183
sebaflorek <sebaflorek@gmail.com>
2022-06-14 11:43:26 +0200

    New methods added to TaskManager

--- src/main/java/pl/coderslab/TaskManager.java
+++ src/main/java/pl/coderslab/TaskManager.java
@@ -1,16 +1,22 @@
 package pl.coderslab;
 
+import org.apache.commons.lang3.ArrayUtils;
+
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.util.Arrays;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class TaskManager {
 
     static final String[] options = {"add", "remove", "list", "exit"};
-    static final String file_name = "tasks_test.csv";
+    static final String file_name = "tasks.csv";
     static String[][] tasks;
 
     // ======================= MAIN ============================
@@ -26,15 +32,16 @@ public class TaskManager {
             String dataInput = scanner.nextLine();
             switch (dataInput) {
                 case "add":
-                    System.out.println("METODA ADD W BUDOWIE");
+                    addTask();
                     break;
                 case "remove":
-                    System.out.println("METODA REMOVE W BUDOWIE");
+                    removeTask();
                     break;
                 case "list":
                     printTaskTab(tasks);
                     break;
                 case "exit":
+                    saveTasksToFile(file_name, tasks);
                     System.out.println(ConsoleColors.RED + "Bye, bye.");
                     System.exit(0);
                     break;
@@ -79,29 +86,20 @@ public class TaskManager {
         System.out.println(reader);
     }
 
-    // KONWERSJA PLIKU DO TABLICY
+    // WYDRUKOWANIE ZAPISANEGO PLIKU
     public static String[][] fileToTab(String fileName) {
-        //Path path = Paths.get(file_name);
-        //if (!Files.exists(path)) {
-        //    System.out.println(ConsoleColors.RED + "File not found" + ConsoleColors.RESET);
-        //}
         File file = new File(fileName);
         int counter = 0;
         String[] singleTask;
+        String[][] tasksTab = new String[0][0];
         try {
             Scanner scan = new Scanner(file);
             while (scan.hasNextLine()) {
                 scan.nextLine();
                 counter += 1;
             }
-            //System.out.println(counter);
-            //System.out.println(Arrays.toString(singleTask));
-        } catch (FileNotFoundException e) {
-            System.out.println(ConsoleColors.RED + "File not found" + ConsoleColors.RESET);
-        }
-        String[][] tasksTab = new String[counter][3];
-        try {
-            Scanner scan = new Scanner(file);
+            tasksTab = new String[counter][3];
+            scan = new Scanner(file);
             while (scan.hasNextLine()) {
                 for (int i = 0; i < counter; i++) {
                     String line = scan.nextLine();
@@ -109,6 +107,8 @@ public class TaskManager {
                     tasksTab[i] = singleTask;
                 }
             }
+            //System.out.println(counter);
+            //System.out.println(Arrays.toString(singleTask));
         } catch (FileNotFoundException e) {
             System.out.println(ConsoleColors.RED + "File not found" + ConsoleColors.RESET);
         }
@@ -116,6 +116,7 @@ public class TaskManager {
         return tasksTab;
     }
 
+    // KONWERSJA PLIKU DO TABLICY[][]
     public static void printTaskTab(String[][] tab) {
         for (int i = 0; i < tab.length; i++) {
             System.out.print(i + " : ");
@@ -124,6 +125,65 @@ public class TaskManager {
             }
             System.out.println();
         }
+    }
+
+    // DODANIE ZADANIA
+    public static void addTask() {
+        Scanner scanAdd = new Scanner(System.in);
+        System.out.println("Please describe the new task:");
+        String newDescription = scanAdd.next();
+        System.out.println("Please add a due date for new task <year-month-day>");
+        String newDate = scanAdd.next();
+        System.out.println("Is the task important? Write true or false");
+        String isImportant = scanAdd.next();
+        tasks = Arrays.copyOf(tasks, tasks.length + 1);
+        tasks[tasks.length - 1] = new String[3];
+        tasks[tasks.length - 1][0] = newDescription;
+        tasks[tasks.length - 1][1] = newDate;
+        tasks[tasks.length - 1][2] = isImportant;
+        System.out.println(ConsoleColors.YELLOW + "Added new task. What do you want to do next?" + ConsoleColors.RESET);
+    }
+
+    // USUWANIE ZADANIA
+    public static void removeTask() {
+        Scanner scanRemove = new Scanner(System.in);
+        System.out.println("Please select a task number to be removed");
+        int taskNumber = -1;
+        try {
+            taskNumber = scanRemove.nextInt();
+        } catch (InputMismatchException e) {
+            System.out.println(ConsoleColors.RED + "Not a number. Try again." + ConsoleColors.RESET);
+            removeTask();
+        }
+        if (taskNumber >= 0) {
+            try {
+                tasks = ArrayUtils.remove(tasks, taskNumber);
+                System.out.println(ConsoleColors.YELLOW + "Task " + taskNumber + " removed. What do you want to do next?" + ConsoleColors.RESET);
+            } catch (IndexOutOfBoundsException ex) {
+                System.out.println(ConsoleColors.RED + "Task number " + taskNumber + " does not exist. Try again." + ConsoleColors.RESET);
+                removeTask();
+            }
+        }
+    }
+
+    // WYJŚCIE Z PROGRAMU
+    public static void saveTasksToFile(String filename, String[][] tab) {
+        Path dir = Paths.get(filename);
+        Path backupDir = Paths.get("backup.csv");
+        // Tworzymy backup :)
+        try {
+            Files.copy(dir, backupDir, StandardCopyOption.REPLACE_EXISTING);
+        } catch (IOException e) {
+            System.err.println(e);
+        }
+
+        String[] lines = new String[tab.length];
+        for (int i = 0; i < tab.length; i++) {
+            lines[i] = String.join(", ", tab[i]);
+        }
+        System.out.println(Arrays.deepToString(tasks));
+
+
     }
 
 
